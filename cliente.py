524a09a03014b139c9665e6b918069261db09acd
nicolaspino <62319198+xiphos-dev@users.noreply.github.com>
2023-03-27 17:51:15 -0300

    Ajustes a la API

--- cliente.py
+++ cliente.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 URL = 'http://127.0.0.1:5000/predecir/'
 RUTA_DATOS = "dataset_SCL.csv"
-N=1
+N=10
 datos = pd.read_csv(RUTA_DATOS, nrows=N).to_json()
 HEADERS = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
 r = requests.post(URL, data=datos, headers=HEADERS)
--- post.lua
+++ post.lua
@@ -1,3 +1,3 @@
 wrk.method = "POST"
-wrk.body   = '{"DIA":{"0":1},"MES":{"0":1},"TIPOVUELO":{"0":"I"},"OPERA":{"0":"American Airlines"},"SIGLADES":{"0":"Miami"}}'
+wrk.body   = '{"MES":{"0":1},"TIPOVUELO":{"0":"I"},"OPERA":{"0":"American Airlines"},"SIGLADES":{"0":"Miami"}}'
 wrk.headers["Content-Type"] = "application/json"
